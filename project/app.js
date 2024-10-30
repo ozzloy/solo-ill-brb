a43bdd547dd7b7ee8c03930c0f7cda402e1f8c9c
aarongarciacr <aarongarcia4997@gmail.com>
2024-10-30 13:43:24 -0600

    Handle errors on duplicate username or email

--- project/app.js
+++ project/app.js
@@ -8,15 +8,45 @@ const { User } = require("./db/models");
 app.use(express.json());
 
 app.post("/users/signup", async (req, res) => {
-  const { firstName, lastName, email, username, password_hash } = req.body;
-  const user = await User.create({
-    firstName,
-    lastName,
-    email,
-    username,
-    password_hash,
-  });
-  res.json(user);
+  try {
+    const { firstName, lastName, email, username, password_hash } = req.body;
+    const user = await User.create({
+      firstName,
+      lastName,
+      email,
+      username,
+      password_hash,
+    });
+    res.json(user);
+  } catch (error) {
+    let emailExists = false;
+    let userNameExists = false;
+
+    error.errors.forEach((error) => {
+      if (error.message === "username must be unique") {
+        userNameExists = true;
+      }
+
+      if (error.message === "email must be unique") {
+        emailExists = true;
+      }
+    });
+
+    let resObj = {
+      message: "User already exists",
+      errors: {},
+    };
+
+    if (emailExists) {
+      resObj.errors["email"] = "User with that email already exists";
+    }
+
+    if (userNameExists) {
+      resObj.errors["username"] = "User with that username already exists";
+    }
+
+    res.status(500).json(resObj);
+  }
 });
 
 app.get("/", (req, res) => {
