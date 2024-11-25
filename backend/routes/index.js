8dec018d79a1d66bb4be293bb365946c9cf53b52
daniel watson <ozzloy@each.do>
2024-11-25 12:05:07 -0800

    deploy: phase 1: set up express backend

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,9 +1,6 @@
-// backend/routes/index.js
 const express = require("express");
 const router = express.Router();
 
-// backend/routes/index.js
-// ...
 const apiRouter = require("./api");
 
 router.use("/api", apiRouter);
@@ -15,7 +12,7 @@ if (process.env.NODE_ENV === "production") {
   // Serve the frontend's index.html file at the root route
   router.get("/", (req, res) => {
     res.cookie("XSRF-TOKEN", req.csrfToken());
-    return res.sendFile(
+    res.sendFile(
       path.resolve(__dirname, "../../frontend", "dist", "index.html"),
     );
   });
@@ -36,8 +33,9 @@ if (process.env.NODE_ENV === "production") {
 // Add a XSRF-TOKEN cookie in development
 if (process.env.NODE_ENV !== "production") {
   router.get("/api/csrf/restore", (req, res) => {
-    res.cookie("XSRF-TOKEN", req.csrfToken());
-    return res.json({});
+    const csrfToken = req.csrfToken();
+    res.cookie("XSRF-TOKEN", csrfToken);
+    res.status(200).json({ "XSRF-Token": csrfToken });
   });
 }
 
