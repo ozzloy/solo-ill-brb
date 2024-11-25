c74239fdd65fa755dfde118ef5222fab1a7efa71
daniel watson <ozzloy@each.do>
2024-11-24 17:01:17 -0800

    phase 3: remove dead logout logic from Navigation, it's in Profile

--- frontend/src/components/Navigation/Navigation.jsx
+++ frontend/src/components/Navigation/Navigation.jsx
@@ -1,16 +1,9 @@
 import { NavLink } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import ProfileButton from "./ProfileButton";
-import * as sessionActions from "../../store/session";
 
 function Navigation({ isLoaded }) {
-  const dispatch = useDispatch();
   const sessionUser = useSelector((state) => state.session.user);
-
-  const logout = (e) => {
-    e.preventDefault();
-    dispatch(sessionActions.logout());
-  };
 
   const sessionLinks = sessionUser ? (
     <>
