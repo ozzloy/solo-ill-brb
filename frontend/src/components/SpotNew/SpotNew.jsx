932c1c688ef48bdaa1dc9fef5d1d3691c8bd5037
daniel watson <ozzloy@each.do>
2024-11-29 10:54:25 -0800

    make new spot form

--- frontend/src/components/SpotNew/SpotNew.jsx
+++ frontend/src/components/SpotNew/SpotNew.jsx
@@ -1,2 +1,11 @@
-const SpotNew = () => <h2>new spot page</h2>;
+import style from "./SpotNew.module.css";
+
+const SpotNew = () => (
+  <form className={style.form}>
+    <h2>new spot</h2>
+    <input className={style.input} placeholder="spot name" />
+    <input className={style.input} placeholder="spot name" />
+    <button>submit</button>
+  </form>
+);
 export default SpotNew;
