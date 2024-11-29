cd4df30b99ba0551fba95e93cd653a5e7055d6fe
daniel watson <ozzloy@each.do>
2024-11-29 12:49:24 -0800

    add first section to form for location details

--- frontend/src/components/SpotNew/SpotNew.jsx
+++ frontend/src/components/SpotNew/SpotNew.jsx
@@ -2,10 +2,55 @@ import style from "./SpotNew.module.css";
 
 const SpotNew = () => (
   <form className={style.form}>
-    <h2>new spot</h2>
-    <input className={style.input} placeholder="spot name" />
-    <input className={style.input} placeholder="spot name" />
-    <button>submit</button>
+    <h2 className={style.h2}>Create a New Spot</h2>
+    {/**
+     * The first section should include: a heading of "Where's your
+     * place located?", a caption of "Guests will only get your exact
+     * address once they booked a reservation.", and text inputs with
+     * labels and placeholders for "Country", "Street Address",
+     * "City", and "State" ("Latitude" and "Longitude" inputs are
+     * optional for MVP)
+     */}
+    <h3 className={style.h3}>Where's your place located?</h3>
+    <p>
+      Guests will only get your exact address once they booked a
+      reservation.
+    </p>
+    <div className={style.inputs}>
+      <div className={style.row}>
+        <label>Country</label>
+        <input className={style.input} placeholder="Country" />
+      </div>
+      <div className={style.row}>
+        <label>Street Address</label>
+        <input className={style.input} placeholder="Street Address" />
+      </div>
+      <div className={style.row}>
+        <label>City</label>
+        <input className={style.input} placeholder="City" />
+      </div>
+      <div className={style.row}>
+        <label>State</label>
+        <input className={style.input} placeholder="State" />
+      </div>
+      <div className={style.row + " " + style.future}>
+        <label>Latitude</label>
+        <input
+          className={style.input}
+          placeholder="future feature Latitude"
+          disabled={true}
+        />
+      </div>
+      <div className={style.row + " " + style.future}>
+        <label>Longitude</label>
+        <input
+          className={style.input}
+          placeholder="future feature Longitude"
+          disabled={true}
+        />
+      </div>
+    </div>
+    <button className={style.button}>Submit</button>
   </form>
 );
 export default SpotNew;
