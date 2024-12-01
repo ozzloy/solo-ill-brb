0fd15431a217473a4e7d394aeefaf045f1a70cab
daniel watson <ozzloy@each.do>
2024-12-01 13:43:04 -0800

    title review form "How was your stay?"

--- frontend/src/components/ReviewFormModal/ReviewFormModal.jsx
+++ frontend/src/components/ReviewFormModal/ReviewFormModal.jsx
@@ -1,9 +1,13 @@
 import style from "../style/Form.module.css";
-
 function ReviewFormModal() {
+  /**
+   * On the new review form, there should be a title at the top with
+   * the text "How was your stay?".
+   */
   return (
     <form className={style.form}>
-      <h1 className={style.h1}>review form modal title</h1>
+      <h1 className={style.h1}>How was your stay?</h1>
+
       <input className={style.input} required />
     </form>
   );
