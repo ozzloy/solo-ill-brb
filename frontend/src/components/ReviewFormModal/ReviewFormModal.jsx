318cd8993e9b556c85ce340f205f3f060575dc23
daniel watson <ozzloy@each.do>
2024-12-01 15:16:13 -0800

    add textarea input and review modal submit button

--- frontend/src/components/ReviewFormModal/ReviewFormModal.jsx
+++ frontend/src/components/ReviewFormModal/ReviewFormModal.jsx
@@ -3,12 +3,23 @@ function ReviewFormModal() {
   /**
    * On the new review form, there should be a title at the top with
    * the text "How was your stay?".
+
+   * There should be a comment text area with a placeholder of "Leave
+   * your review here...".
    */
   return (
     <form className={style.form}>
       <h1 className={style.h1}>How was your stay?</h1>
 
-      <input className={style.input} required />
+      <textarea
+        className={style.input}
+        placeholder="Leave your review here..."
+        required
+      />
+
+      <button className={style.button} type="button">
+        Submit Your Review
+      </button>
     </form>
   );
 }
